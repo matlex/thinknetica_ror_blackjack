f7ee13472818e13d4b9fbc2c5081ba0a000514ed
Matvey Ustinov <mat.ustinov@gmail.com>
2018-11-06 09:03:05 +0000

    Вернул значение константы INITIAL_PLAYER_BANK_AMOUNT на 100 (как того требуют условия задачи)

--- constants.rb
+++ constants.rb
@@ -5,5 +5,5 @@ module Constants
     'A' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9,
     'T' => 10, 'J' => 10, 'Q' => 10, 'K' => 10
   }
-  INITIAL_PLAYER_BANK_AMOUNT = 20
+  INITIAL_PLAYER_BANK_AMOUNT = 100
 end
